5ec87da6dbee18296c644fb477dfd29c14632ee1
Karina Milovanova <31618399+carusalgiz@users.noreply.github.com>
2017-11-10 00:02:55 +1000

    Initial Commit

---
+++ task1/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Даны два целых числа, отличных от нуля. Найдите их сумму, произведение, разность, частное.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            var num1 = Console.ReadLine();
+            Console.WriteLine("Введите второе число:");
+            var num2 = Console.ReadLine();
+            double a = double.Parse(num1);
+            double b = double.Parse(num2);
+
+            double summary = a + b;
+            double substraction = a - b;
+            double multiplication = a * b;
+            double division = a / b;
+
+            Console.WriteLine("Сумма чисел: " + summary);
+            Console.WriteLine("Разность чисел: " + substraction);
+            Console.WriteLine("Произведение чисел: " + multiplication);
+            Console.WriteLine("Частное чисел: " + division);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task2/task2/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Стоимость минуты разговора по телефону — P руб. 
+ * Если продолжительность разговора превышает 5 минут, то на оставшуюся часть времени действует скидка, равная 20%. 
+ * Написать программу, определяющую стоимость разговора заданной продолжительности T.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        public double Sale(double price, int time)
+        {
+            int sec = time - 300;
+            if ((sec % 60) != 0)
+            {
+                return ((5 * price) + (((sec / 60) + 1) * price) * 0.2);             
+            }
+            else
+            {
+                return ((5 * price) + ((sec / 60) * price) * 0.2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            double P, result;
+            int T;
+            Console.WriteLine("Введите стоимость минуты разговора: ");
+            var p = Console.ReadLine();
+            Console.WriteLine("Введите продолжительность разговора (в секундах): ");
+            var t = Console.ReadLine();
+            P = double.Parse(p);
+            T = int.Parse(t);
+
+            if(T > 300)
+            {
+                result = prog.Sale(P, T);
+            }
+            else
+            {
+                result = (T / 60) * P;
+            }
+            Console.WriteLine("Стоимость разговора: " + result);
+            Console.ReadKey();
+        }
+    }
+}
